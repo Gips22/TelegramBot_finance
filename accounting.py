f7e1c422d7a1c3105e1c17aa9cb38d381ea0b0f7
andrey <gipsme123@gmail.com>
2022-12-23 13:25:49 +0300

    Добавил функции для расчета статистики в бизнес-логику accounting, также добавил функции в обертку для работы с БД

--- accounting.py
+++ accounting.py
@@ -41,7 +41,58 @@ def _parse_message(raw_message: str) -> Message:
     return Message(amount=amount, category_text=category_text)
 
 
+def _get_now_formatted() -> str:
+    """Возвращает сегодняшнюю дату строкой"""
+    return _get_now_datetime().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def _get_now_datetime() -> datetime.datetime:
+    """Возвращает сегодняшний datetime с учётом времненной зоны Мск."""
+    tz = pytz.timezone("Europe/Moscow")
+    now = datetime.datetime.now(tz)
+    return now
+
 def add_expense(raw_message: str) -> Expense:
     """Добавляет и сохраняет в БД новый расход. Принимает на вход сообщение из бота."""
     parsed_message = _parse_message(raw_message)
-    category = Categories().get_category()
+    category = Categories().get_category(parsed_message.category_text)
+
+    inserted_row_id = db.insert("expense", {
+        "amount": parsed_message.amount,
+        "created": _get_now_formatted(),
+        "category_codename": category.codename,
+        "raw_text": raw_message
+    })
+    return Expense(id=None, amount=parsed_message.amount, category_name=category.name)
+
+
+def _get_budget_limit():
+    """Возвращает дневной лимит трат для основных базовых трат"""
+    return db.fetchall("budget", ["daily_limit"])[0]["daily_limit"]  # TODO: тут понять индекс [0]["daily_limit"]
+
+
+def get_today_statistics() -> str:
+    """Возвращает статистику за сегодня"""
+    cursor = db.get_cursor()
+    cursor.execute("select sum(amount)"
+                   "from expense where date(created)=date('now', 'localtime')")
+    result = cursor.fetchone()
+    if not result[0]:
+        return "За сегодня расходов нет"
+    all_today_expenses = result[0]
+    cursor.execute("select sum(amount) "
+                   "from expense where date(created)=date('now', 'localtime') "
+                   "and category_codename in (select codename "
+                   "from category where is_base_expense=true)")
+    result = cursor.fetchone()
+    base_today_expenses = result[0] if result[0] else 0
+    return (f"Расходы сегодня: \n"
+            f"всего - {all_today_expenses} руб. \n"
+            f"базовые — {base_today_expenses} руб. из {_get_budget_limit()} руб.\n\n"
+            f"За текущий месяц: /month")
+
+def get_month_statistics() -> str:
+    """Возвращает строкой статистику расходов за текущий месяц"""
+    now = _get_now_datetime()
+    first_day_of_month = f'{now.year:04d}-{now.month:02d}-01'  # TODO: посмотреть как считается данная формула
+
--- db.py
+++ db.py
@@ -19,7 +19,8 @@ def insert(table: str, column_values: Dict):
         values)
     connection.commit()
 
-def fetchall(table:str, columns: List[str]) -> List[Tuple]:
+
+def fetchall(table: str, columns: List[str]) -> List[Tuple]:
     columns_joined = ', '.join(columns)
     cursor.execute(f"select {columns_joined} from {table}")
     rows = cursor.fetchall()
@@ -30,3 +31,34 @@ def fetchall(table:str, columns: List[str]) -> List[Tuple]:
             dict_row[column] = row[index]
         rez.append(dict_row)
     return rez
+
+
+def delete(table: str, row_id: int) -> None:
+    row_id = int(row_id)
+    cursor.execute(f"delete from {table} where id={row_id}")
+    connection.commit()
+
+
+def get_cursor():
+    return cursor
+
+
+def _init_db():
+    """Инициализация БД"""
+    with open("createdb.sql", "r") as f:
+        sql = f.read()
+        cursor.executescript(sql)
+        connection.commit()
+
+
+def check_db_exists():
+    """Проверяет, инициализирована ли БД, если нет - инициализирует"""
+    cursor.execute("select name from sqlite_master "
+                   "where type='table' and name='expense'")  # sqlite_master встроенная таблица в sqlite, в которой содержится инфа о таблицах в текущей бд
+    table_exist = cursor.fetchall()
+    if table_exist:
+        return
+    _init_db()
+
+
+check_db_exists()
